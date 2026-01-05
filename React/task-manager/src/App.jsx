d4bc4003d40aeeb86cebe419e09efd4a947bf520
SathvikSesha <lakshmisarmatk@gmail.com>
2026-01-05 19:05:24 +0530

    Added Brower Routes to App.jsx

--- React/task-manager/src/App.jsx
+++ React/task-manager/src/App.jsx
@@ -1,3 +1,14 @@
-function App() {}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TaskHome from "./TaskHome";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TaskHome />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;

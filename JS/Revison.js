2df6b03b82a732f6f7d688aa3cefb92ee1e9f084
SathvikSesha <lakshmisarmatk@gmail.com>
2026-04-10 15:33:22 +0530

    feat: add JS/Revison.js

---
+++ JS/Revison.js
@@ -0,0 +1,71 @@
+// let x = 20;
+// let y = 10;
+// console.log(x + y);
+
+// for (let x = 0; x < 3; x++) {
+//   console.log(x + " ");
+// }
+// console.log(x);
+
+// const repeat = (a, b) => {
+//   for (let i = 0; i < a; i++) {
+//     console.log(b);
+//   }
+// };
+
+// repeat(10, 5);
+
+// const product = async () => {
+//   const response = await fetch("http://localhost:4000/posts/get");
+//   const data = await response.json();
+//   console.log(data);
+// };
+
+// product();
+
+// const user1 = {
+//   name: "sathvik",
+//   age: 20,
+// };
+// const user2 = {
+//   ...user1,
+//   skills: ["java", "java script"],
+// };
+// console.log(user2);
+
+// let a = 100;
+// let b = 20;
+// const add = () => {
+//   return a + b;
+// };
+// console.log(add());
+
+// let a = [1, 3, 2, 6, 4, 3];
+// let b = a
+//   .filter((num) => num % 2 == 0)
+//   .map((num) => num * 2)
+//   .reduce((sum, num) => sum + num, 0);
+// console.log(b);
+
+// class Person {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//   greet = () => {
+//     console.log(this.name);
+//   };
+// }
+// const p = new Person("Sathvik");
+// p.greet();
+// let arr = [1, 2, 3, 4, 5];
+// for (let i of arr) {
+//   console.log(i);
+// }
+// let set = new Set();
+// set.add(2);
+// set.add(4);
+// set.add(3);
+// set.add(4);
+// console.log(set);
+// console.log(set.delete(4));
+// console.log(set)
